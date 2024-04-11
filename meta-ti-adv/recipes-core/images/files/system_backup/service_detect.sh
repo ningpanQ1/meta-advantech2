63e1cae96c9a386f3fe58f26600c5cd462c13246
ning.pan <ning.pan@advantech.com.cn>
2024-04-11 17:34:58 +0800

    Fixup some errors.

--- meta-ti-adv/recipes-core/images/files/system_backup/service_detect.sh
+++ meta-ti-adv/recipes-core/images/files/system_backup/service_detect.sh
@@ -4,25 +4,25 @@ sleep 10
 
 flag1=`systemctl list-units |grep systemd-logind |awk {'print $3'}`
 if [ x$flag1 != "xactive" ];then
-	echo "[ADV] systemd-logind boot fail" > /dev/ttyO0
+	echo "[ADV] systemd-logind boot fail" > /dev/ttyS0
 fi
-flag2=`systemctl list-units |grep weston |awk {'print $3'}`
+flag2=`systemctl list-units |grep weston.service |awk {'print $3'}`
 if [ x$flag2 != "xactive" ];then
-    echo "[ADV] weston boot fail" > /dev/ttyO0
+    echo "[ADV] weston boot fail" > /dev/ttyS0
 fi
-flag3=`systemctl list-units |grep serial-getty@ttyO0.service |awk {'print $3'}`
+flag3=`systemctl list-units |grep serial-getty@ttyS0.service |awk {'print $3'}`
 if [ x$flag3 != "xactive" ];then
-    echo "[ADV] serial-getty@ttyO0.service boot fail" > /dev/ttyO0
+    echo "[ADV] serial-getty@ttyS0.service boot fail" > /dev/ttyS0
 fi
 
 
 if [[ x$flag1 == "xactive" ]] && [[ x$flag2 == "xactive" ]] && [[ x$flag3 == "xactive" ]];then
     # disable WDT
-    echo "[ADV] service boot success"  > /dev/ttyO0
+    echo "[ADV] service boot success"  > /dev/ttyS0
     if [ -d "/sys/class/adv_bootprocess_class" ];then
         echo 1 > /sys/class/adv_bootprocess_class/adv_bootprocess_device/timer_flag
     fi
 else
-    echo "[ADV] service boot fail" > /dev/ttyO0
+    echo "[ADV] service boot fail" > /dev/ttyS0
 fi
 
